497a2ff124f31c890413ee6b3ea8a2f0076ab271
DiWang <wangdi931010@gmail.com>
2019-10-18 17:43:14 +0200

    improved read function, write a new class for read, finish stage 1

--- LIGGGHTSER/read.py
+++ LIGGGHTSER/read.py
@@ -3,37 +3,146 @@
 import os
 import linecache
 
-def read_file(dirname):
-	filenames=os.listdir(dirname)
-	filedict = dict()
-	filedict['dump'] = list()
-	filedict['contact'] = list()
-	filedict['log'] = list()
-	filedict['print'] = list()
-	filedict['ave'] = list()
-	for name in filenames:
-		with open(name,'r') as file:
-		# file=open(name)
-			line = file.readline()
-			if line.startswith('ITEM'):
-				text = linecache.getline(name,3)
-				if text.startswith('ITEM: NUMBER OF ATOM'):
-					filedict['dump'].append(name)
-					continue
-				if text.startswith('ITEM: NUMBER OF ENTRIES'):
-					filedict['contact'].append(name)
-					continue
-			if line.startswith('LIGGGHTS'):
-				filedict['log'].append(name)
-				break
-			if line.startswith('# Fix print output for fix'):
-				filedict['print'].append(name)
-				break
-			if line.startswith('# Time-averaged data for fix'):
-				filedict['ave'].append(name)
-				break
-		# file.close()
-	return filedict
+class Read:
+	def __init__(self, name, version):
+		self.name=name
+		self.version=version
 
+	def read_file(self, dirname):
+		filenames=list()
+		for root, dirs, files in os.walk(dirname):
+			files = [f for f in files if not f[0] == '.']
+			dirs = [d for d in dirs if not d[0] == '.']
+			for names in files:
+				filenames.append(os.path.join(root,names))
+		filedict = dict()
+		filedict['dump'] = list()
+		filedict['contact'] = list()
+		filedict['log'] = list()
+		filedict['print'] = list()
+		filedict['ave'] = list()
+		filedict['others'] = list()
+		for name in filenames:
+			with open(name,'r') as file:
+				try:
+					line = file.readline()
+				except:
+					filedict['others'].append(name)
+					continue
+				if line.startswith('ITEM'):
+					text = linecache.getline(name,3)
+					if text.startswith('ITEM: NUMBER OF ATOM'):
+						filedict['dump'].append(name)
+						continue
+					if text.startswith('ITEM: NUMBER OF ENTRIES'):
+						filedict['contact'].append(name)
+						continue
+				if line.startswith('LIGGGHTS'):
+					filedict['log'].append(name)
+					continue
+				if line.startswith('# Fix print output for fix'):
+					filedict['print'].append(name)
+					continue
+				if line.startswith('# Time-averaged data for fix'):
+					filedict['ave'].append(name)
+					continue
+				filedict['others'].append(name)
+		return filedict
+
+	def read_dump(self, fname):
+	    mode = str()
+	    dump = open(fname)
+	    line = dump.readline()
+	    while line.startswith('ITEM') :
+	        snap = dict()
+	        if line.startswith('ITEM: TIMESTEP'):
+	            line = dump.readline()
+	            snap['TIMESTEP'] = int(line.split()[0])
+	            line = dump.readline()
+	        if line.startswith('ITEM: NUMBER OF ATOM'):
+	            line = dump.readline()
+	            snap['NUMBER_OF_ATOM'] = int(line.split()[0])
+	            line = dump.readline()
+	        if line.startswith('ITEM: BOX'):
+	            snap['BOUNDARY_CONDITIONN'] = line.split()[-3:]
+	            line = dump.readline()
+	            snap['BOUNDARY_X'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+	            snap['BOUNDARY_Y'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+	            snap['BOUNDARY_Z'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+
+	        if line.startswith('ITEM: ATOMS'):
+	            snap['HEADER'] = line.split()[2:]
+	            m=snap['HEADER']
+	            print(m)
+	            
+	        snap['DATA'] = list()
+	        for items in range(len(m)):
+	            snap[m[items]] = list()
+
+	        for line in dump:
+	            try:
+	                snap['DATA'].append([float(i) for i in line.split()])
+	            except:
+	                print('error: illegal dump file, data end with words')
+	                break
+
+	        for i in range(len(snap['DATA'])):
+	                    for items in range(len(m)):
+	                        snap[m[items]].append(snap['DATA'][i][items])
+	    return snap
+
+	def read_contact(self, fname):
+	    mode = str()
+	    dump = open(fname)
+	    line = dump.readline()
+
+	    while line.startswith('ITEM') :
+	        snap = dict()
+	        if line.startswith('ITEM: TIMESTEP'):
+	            line = dump.readline()
+	            snap['TIMESTEP'] = int(line.split()[0])
+	            line = dump.readline()
+	        if line.startswith('ITEM: NUMBER OF ENTRIES'):
+	            line = dump.readline()
+	            snap['ITEM: NUMBER OF ENTRIES'] = int(line.split()[0])
+	            line = dump.readline()
+	        if line.startswith('ITEM: BOX BOUNDS'):
+	            snap['BOUNDARY_CONDITIONN'] = line.split()[-3:]
+	            line = dump.readline()
+	            snap['BOUNDARY_X'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+	            snap['BOUNDARY_Y'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+	            snap['BOUNDARY_Z'] = [float(i)
+	                                  for i in line.split()]
+	            line = dump.readline()
+
+	        if line.startswith('ITEM: ENTRIES'):
+	            snap['HEADER'] = line.split()[2:]
+	            m=snap['HEADER']
+	            print(m)
+	            
+	        snap['DATA'] = list()
+	        for items in range(len(m)):
+	            snap[m[items]] = list()
+
+	        for line in dump:
+	            try:
+	                snap['DATA'].append([float(i) for i in line.split()])
+	            except:
+	                print('error: illegal contact file, data end with words')
+
+	        for i in range(len(snap['DATA'])):
+	                    for items in range(len(m)):
+	                        snap[m[items]].append(snap['DATA'][i][items])
+	    return snap
 
 
--- backup/dump.py
+++ backup/dump.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__auther__ = 'Roy Kid'
 
 # A middle teir that can easily format data of lammps's dump
 # and provide a serise of interfaces to access.
@@ -10,91 +9,54 @@ __auther__ = 'Roy Kid'
 import re
 import glob
 
-oneline = 'Read LAMMPS dump files and extract position data'
-docstr = """
-    This module is used to extract position data from lammps dump.
-"""
+def read_data(fname):
+    mode = str()
+    dump = open(fname)
+    line = dump.readline()
 
-# Variables
-# _properties : store box properties
-#		[pro1, pro2...]
-#                pro1 : {TIMESTEP:1000, NUBER_OF_ATOM:1000, ...}
-#
-# _field : store header
-#	   [fie1, fie2...]
-#          fie1 : {id:0, mol:1, type:2, ...}
-#
-# _data : store position
-#	  [snap1, snap2,...]
-#         snap1 : [[x0,y0,z0,...],
-#                  [x1,y1,z1,...],...]
+    print("function strat")
+    while line.startswith('ITEM') :
+        snap = dict()
+        if line.startswith('ITEM: TIMESTEP'):
+            line = dump.readline()
+            snap['TIMESTEP'] = int(line.split()[0])
+            line = dump.readline()
+        if line.startswith('ITEM: NUMBER OF ATOM'):
+            line = dump.readline()
+            snap['NUMBER_OF_ATOM'] = int(line.split()[0])
+            line = dump.readline()
+        if line.startswith('ITEM: BOX'):
+            snap['BOUNDARY_CONDITIONN'] = line.split()[-3:]
+            line = dump.readline()
+            snap['BOUNDARY_X'] = [float(i)
+                                  for i in line.split()]
+            line = dump.readline()
+            snap['BOUNDARY_Y'] = [float(i)
+                                  for i in line.split()]
+            line = dump.readline()
+            snap['BOUNDARY_Z'] = [float(i)
+                                  for i in line.split()]
+            line = dump.readline()
 
-# Imports and external programs
+        if line.startswith('ITEM: ATOMS'):
+            snap['HEADER'] = line.split()[2:]
+            m=snap['HEADER']
+            
 
-# Class definition
-
-
-class Dump:
-    def __init__(self, fpath, every=1):
-        self._fname = glob.glob(fpath)[0]
-        self._every = every
-
-    def __iter__(self):
-        return self._read_data(self._fname, self._every)
-
-    def _read_data(self, fname, every):
-        snap_index = 0
-        mode = str()
-        dump = open(fname)
-        line = dump.readline()
-
-        while line.startswith('ITEM') :
-            if snap_index % every == 0:
-                snap = dict()
-                if line.startswith('ITEM: TIMESTEP'):
-                    line = dump.readline()
-                    snap['TIMESTEP'] = int(line.split()[0])
-                    line = dump.readline()
-                if line.startswith('ITEM: NUMBER OF ATOM'):
-                    line = dump.readline()
-                    snap['NUMBER_OF_ATOM'] = int(line.split()[0])
-                    line = dump.readline()
-                if line.startswith('ITEM: BOX'):
-                    snap['BOUNDARY_CONDITIONN'] = line.split()[-3:]
-                    line = dump.readline()
-                    snap['BOUNDARY_X'] = [float(i)
-                                          for i in line.split()]
-                    line = dump.readline()
-                    snap['BOUNDARY_Y'] = [float(i)
-                                          for i in line.split()]
-                    line = dump.readline()
-                    snap['BOUNDARY_Z'] = [float(i)
-                                          for i in line.split()]
-                    line = dump.readline()
-
-                if line.startswith('ITEM: ATOMS'):
-                    snap['HEADER'] = line.split()[2:]
-
-
-                snap['DATA'] = list()
-
-                for line in dump:
-                    try:
-                        snap['DATA'].append([float(i) for i in line.split()])
-                    except:
-                        snap_index += 1
-                        yield snap
-                        break
-            else:
-                for line in dump:
-                    line = dump.readline()
-                    if line.startswith('ITEM: TIMESTEP'):
-                        snap_index += 1
-                        mode = 'every'
-                        break
         
-        if mode != 'every':
-            yield snap
+        snap['DATA'] = list()
+        for items in range(len(m)):
+            snap[m[items]] = list()
+
+        for line in dump:
+            try:
+                snap['DATA'].append([float(i) for i in line.split()])
+            except:
+                for i in range(len(snap['DATA'])):
+                    for items in range(len(m)):
+                        snap[m[items]].append(snap['DATA'][i][items])
+                return snap
+                break
 
 
                
