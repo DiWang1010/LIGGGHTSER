2080ed07ab51d3df5f2047207a6403f43213f644
DiWang <wangdi931010@gmail.com>
2020-01-30 10:23:13 +0100

    fix del bug & reconnect module

--- LIGGGHTSER/ob.py
+++ LIGGGHTSER/ob.py
@@ -20,9 +20,19 @@ class ob:
 		self.ssh.connect(ip,port=22,username=user,password=psd)
 		print('Connection successful')
 		self.client=self.ssh.get_transport()
-		# except:
-		# 	print('Connection fail')
+		self.ssh.close()
+		self.ip=ip
+		self.user=user
+		self.psd=psd
+		
+	def reconnect(self):
+		self.ssh=SSHClient()
+		self.ssh.load_system_host_keys()
+		self.ssh.connect(self.ip,port=22,username=self.user,password=self.psd)
+		self.client=self.ssh.get_transport()
+
 	def squeue(self):
+		self.reconnect()
 		stdin,stdout,stderr = self.ssh.exec_command('squeue')
 		out=stdout.read()
 		outtemp=str(out)
@@ -32,9 +42,11 @@ class ob:
 		for i in range(lines):
 			line_temp=stdout.readline()
 			line_temp=str(line_temp)
-			line_temp=line_temp.replace('\n','')
+			# line_temp=line_temp.replace('\n','')
 			outline.append(line_temp.split())
+			outline[i].append('\n')
 		print(outline)
+		self.ssh.close()
 		return outline
 
 	def email_set(self,user,psd,smpt_server,port,target,test_mail=False):
@@ -73,14 +85,19 @@ class ob:
 		# msg['To'] =  COMMASPACE.join(target)
 		msg['To'] =  email[4]
 		msg['Subject'] ='Job info mail'
-		msg_text= MIMEText(strings,'html','utf-8')
+		msg_text= MIMEText(strings,'plain','utf-8')
 		msg.attach(msg_text)
 		server.sendmail(email[0],[email[4]],msg.as_string())
 
 	def monitor(self,jobid=None,time_gap=60,email=None,file=None):
+		squeue_data=self.squeue()
+		strings=[' '.join(squeue_data[i]) for i in range(len(squeue_data))]
+		strings='\n'.join(strings)
+		self.send_email(email,strings)
 		while 1:
 			squeue_data=self.squeue()
 			check_list=[0 for i in range(len(jobid))]
+			shrink_flag=0
 			for i in range(len(jobid)):
 				for j in range(len(squeue_data)):
 					# print(jobid[i])
@@ -96,11 +113,14 @@ class ob:
 					self.send_email(email,strings)
 					if(file):
 						pass
-					del jobid[i]
+					jobid[i]=0
+					shrink_flag=1
 				else:
 					print('Job'+str(jobid[i])+'down.')
-					del jobid[i]
-			# print(jobid)
+					jobid[i]=0
+					shrink_flag=1
+			if(shrink_flag==1):
+				jobid=[x for x in jobid if x!=0]
 			if(len(jobid)==0):
 				strings='Jobs all down.Monitor terminated'
 				self.send_email(email,strings)
